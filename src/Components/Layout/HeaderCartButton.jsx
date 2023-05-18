c012abf0a5023d687e361979aa63a10ebd7158bb
magdy <magdy.feteah@gmail.com>
2023-05-19 00:28:18 +0300

    upload ReactMeals

--- src/Components/Layout/HeaderCartButton.jsx
+++ src/Components/Layout/HeaderCartButton.jsx
@@ -1,21 +1,23 @@
-import { useContext ,useEffect ,useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../Store/Cart-Context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  const cartCtx = useContext(CartContext);
   const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-    
+  const cartCtx = useContext(CartContext);
+
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
   const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
 
   useEffect(() => {
-    if (cartCtx.items.length === 0) {
+    if (items.length === 0) {
       return;
     }
     setBtnIsHighlighted(true);
@@ -27,7 +29,7 @@ const HeaderCartButton = (props) => {
     return () => {
       clearTimeout(timer);
     };
-  }, [cartCtx.items]);
+  }, [items]);
 
   return (
     <button className={btnClasses} onClick={props.onClick}>
