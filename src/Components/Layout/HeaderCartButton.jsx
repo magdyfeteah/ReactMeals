72a407dee36702cf281f76474e03419293d0f6f2
magdy <magdy.feteah@gmail.com>
2023-05-19 11:19:05 +0300

    modified

--- src/Components/Layout/HeaderCartButton.jsx
+++ src/Components/Layout/HeaderCartButton.jsx
@@ -1,35 +1,33 @@
-import { useContext, useState } from 'react';
+import { useContext ,useEffect ,useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../Store/Cart-Context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-
-  const { items } = cartCtx;
-
-  const numberOfCartItems = items.reduce((curNumber, item) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    
     return curNumber + item.amount;
   }, 0);
 
   const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
 
-  // useEffect(() => {
-  //   if (items.length === 0) {
-  //     return;
-  //   }
-  //   setBtnIsHighlighted(true);
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
 
-  //   const timer = setTimeout(() => {
-  //     setBtnIsHighlighted(false);
-  //   }, 300);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
 
-  //   return () => {
-  //     clearTimeout(timer);
-  //   };
-  // }, [items]);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
 
   return (
     <button className={btnClasses} onClick={props.onClick}>
